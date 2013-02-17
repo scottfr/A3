0aba315d88910d2a0fb7857af32264599d836f69
Scott Fortmann-Roe <scottfr@gmail.com>
2013-02-16 16:51:06 -0800

    Initial package commit.

---
+++ R/A3.data.R
@@ -0,0 +1,53 @@
+#' Boston Housing Prices
+#' 
+#' A dataset containing the prices of houses in the Boston region and a number of features.
+#' The dataset and the following description are provided by UCI Machine Learning Repository (\url{http://archive.ics.uci.edu/ml/datasets/Housing}).
+#' 
+#' \itemize{
+#'   \item CRIME: per capita crime rate by town 
+#'   \item ZN: proportion of residential land zoned for lots over 25,000 sq.ft. 
+#'   \item INDUS: proportion of non-retail business acres per town 
+#'   \item CHAS: Charles River dummy variable (= 1 if tract bounds river; 0 otherwise) 
+#'   \item NOX: nitric oxides concentration (parts per 10 million) 
+#'   \item ROOMS: average number of rooms per dwelling 
+#'   \item AGE: proportion of owner-occupied units built prior to 1940 
+#'   \item DISTANCE: weighted distances to five Boston employment centres 
+#'   \item HIGHWAY: index of accessibility to radial highways 
+#'   \item TAX: full-value property-tax rate per $10,000 
+#'   \item PUPIL.TEACHER: pupil-teacher ratio by town 
+#'   \item MINORITY: 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town 
+#'   \item LSTAT: % lower status of the population 
+#'   \item MED.VALUE: Median value of owner-occupied homes in $1000's
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name housing
+#' @usage data(housing)
+#' @references
+#' Frank, A. & Asuncion, A. (2010). UCI Machine Learning Repository [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School of Information and Computer Science.
+#' 
+#' Harrison, D. and Rubinfeld, D.L. Hedonic prices and the demand for clean air, J. Environ. Economics & Management, vol.5, 81-102, 1978.
+NULL
+
+#' Ecosystem Multifunctionality
+#' 
+#' This dataset relates multifunctionality to a number of different biotic and abiotic features in a global survey of drylands. The dataset was obtained from (\url{http://www.sciencemag.org/content/335/6065/214/suppl/DC1}). The dataset contains the features listed below.
+#' 
+#' \itemize{
+#'   \item ELE: Elevation of the site
+#'   \item LAT & LONG: Location of the site
+#'   \item SLO: Site slope
+#'   \item SAC: Soil sand content
+#'   \item PCA_C1, PCA_C2, PCA_C3, PCA_C4: Principal components of a large set of primarily climatic features
+#'   \item SR: Species richeness
+#'   \item MUL: Multifunctionality
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name multifunctionality
+#' @usage data(multifunctionality)
+#' @references
+#' Maestre, F. T., Quero, J. L., Gotelli, N. J., Escudero, A., Ochoa, V., Delgado-Baquerizo, M., et al. (2012). Plant Species Richness and Ecosystem Multifunctionality in Global Drylands. Science, 335(6065), 214-218. doi:10.1126/science.1215442
+NULL
